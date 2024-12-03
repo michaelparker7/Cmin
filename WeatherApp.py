560c3ba41846b0d02d113351b3b4cd356db64fd7
Michael Parker <mjp225@lehigh.edu>
2024-12-02 21:56:31 -0500

    eded

--- WeatherApp.py
+++ WeatherApp.py
@@ -8,8 +8,8 @@ geolocator = Nominatim(user_agent="geo_locator")
 st.title("City Coordinates Finder")
 
 # Sidebar for user input
-city = st.sidebar.text_input("Enter a city name:", "New York, USA")
-
+city = st.sidebar.text_input("Enter a city name:", "Bethlehem, PA")
+selected_date = st.sidebar.date_input("Select a date:", None)
 # Fetch coordinates when the user enters a city
 if city:
     location = geolocator.geocode(city)
@@ -19,3 +19,22 @@ if city:
         st.write(f"**Longitude:** {location.longitude}")
     else:
         st.error(f"Could not find coordinates for '{city}'. Please try again.")
+
+
+stations = Stations()
+
+start_date = selected_date
+end_date = selected_date
+
+ohthatstheone = stations.nearby(location.latitude,location.longitude).fetch().head(1)
+ohthatstheone['latitude']
+weather_location = Point(ohthatstheone['latitude'],ohthatstheone['longitude'])
+weather_location
+
+data = Daily(weather_location, start_date, end_date)
+data = data.fetch()
+
+# Print the temperature data
+print(f"Temperature on {start_date.date()}:")
+print(f"Min: {data['tmin'][0]}°C")
+print(f"Max: {data['tmax'][0]}°C")
