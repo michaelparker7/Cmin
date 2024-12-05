07c578fa775a4c47e04492805b1c69e237870bff
Michael Parker <mjp225@lehigh.edu>
2024-12-05 17:04:13 -0500

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -291,7 +291,7 @@ with tab2:
                 ranges of temperatures that are relevant for this assignment. 
                 """
             )
-            matlab_code = """
+            matlab_code1 = """
             function [rho, k, c, alpha, nu, Pr] = ME321ProjectPt4(T)
                % need density rho, thermal conductivity k, heat capacity c, Fourier # Fo,
                % diffusivity alpha (function of rho, k, c), kinematic viscosity nu, Pr
@@ -334,8 +334,156 @@ with tab2:
         
         elif section == "5: Time Marching Function":
             st.subheader("5. Time Marching Function")
-            st.write("This section will describe the time marching function.")
-            # Add more content here as needed.
+            st.write(
+                """
+                Set up a time marching function that uses your result from (1) as the initial condition and
+                proceeds for 48 hours using the explicit forward-marching finite-difference method.
+                Generate one plot showing the temperature distribution in the concrete and ground soil at t
+                = 0 (initial steady-state solution), midnight of the second night (t = 24 hours) and noon of the
+                second day (t = 36 hours), using the same format as in (1). Generate a second plot showing
+                surface temperature, Ts, over the 48-hour period. Complete this step for zip code 18015 and
+                your chosen second location. 
+                """
+            )
+            matlab_code2 = """
+                %Read in initial conditions from input text files;
+                T_init_Vegas = csvread('Vegas_init.txt');
+                T_init_Beth = csvread('Beth_init.txt');
+                ambient_vegas = csvread('Vegas_amb.txt');
+                ambient_Beth = csvread('Beth_amb.txt');
+                solar_flux_vegas = csvread('Vegas_flux.txt');
+                %set program to solve for location: type either T_init_Vegas or T_init_Beth
+                T_init = T_init_Vegas;
+                ambdata = ambient_vegas;
+                fluxdata = solar_flux_vegas;
+                T_dew = 285; %local dew point temp in kelvin
+                N = 0; %percent cloud cover
+                wind_speed = 10; %average windspeed m/s
+                %data and constrats
+                Sidewalk_width = 1;
+                Sidewalk_length = 1;
+                L_re = Sidewalk_width;
+                L_gr = (Sidewalk_width^2)/(2*Sidewalk_width+2*Sidewalk_length);
+                A_sidewalk = Sidewalk_width*Sidewalk_length;
+                %Constant Material Properties
+                nodal_spacing = 0.01;
+                %Dirt
+                k_dirt = 0.52;
+                p_dirt = 2050;
+                cp_dirt = 1840*1000;
+                alpha_dirt = k_dirt/(p_dirt*cp_dirt);
+                %Concrete
+                k_conc = 1.4;
+                p_conc = 2300;
+                cp_conc = 880*1000;
+                alpha_conc = k_conc/(p_conc*cp_conc);
+                emissivity= 0.9;
+                solar_abs = 0.65;
+                %k_dirt = k_conc;
+                %p_dirt = p_conc;
+                %cp_dirt = cp_conc;
+                %alpha_dirt = k_dirt/(p_dirt*cp_dirt);
+                %Node creation
+                nodes = ones(1,115);
+                nodes(1:13) = 3; %Concrete Nodes
+                nodes(13) = 2; %Soil to Concrete Boundary Node
+                nodes(1) = 4; %node exposed to thermal radiation and convection
+                nodes(length(nodes)) = 5; %sets final layer of model
+                %All other nodes at 1
+                %Import air properties as function of time
+                %Loop Through Time
+                t_step = 60; %timestep in seconds
+                time_vect = 0:t_step:48*3600;
+                %Temp vs time matrix setup
+                T = zeros(length(nodes),length(time_vect));
+                T(:,1) = T_init; %sets starting node temp
+                %Joint node
+                Fo_Jdirt = 2*k_dirt * t_step/((p_dirt*cp_dirt+p_conc*cp_conc)*(nodal_spacing^2));
+                Fo_Jconc = 2*k_conc * t_step/((p_dirt*cp_dirt+p_conc*cp_conc)*(nodal_spacing^2));
+                %Fourier number calcs
+                Fo_dirt = alpha_dirt*t_step/(nodal_spacing^2);
+                Fo_conc = alpha_conc*t_step/(nodal_spacing^2);
+                for i = 1:length(time_vect)
+                   %Get air properties at T
+                   %Sky Constants:
+                   %alpha_air = 22.5*10^-6;
+                   %v = 15.89*10^-6; %air kinematic viscosity
+                   %Pr = 0.707;
+                   %k = 26.3*10^-3; %Air thermal conductivity
+                   %T_sky = 60;
+                   [rho, k, c, alpha_air, v, Pr] = air_prop(real(T(1,i)));
+                   %[T_amb] = ambient_temp(i,ambdata)
+                  
+                   %[q_raq_flux] = solarflux(i,fluxdata)
+                   time_hours(i) = i/60;
+                   q_raq_flux = interp1(fluxdata(:,1),fluxdata(:,2),time_hours(i));
+                   T_amb = interp1(ambdata(:,1),ambdata(:,2),time_hours(i));
+                   %Need a function to calculate convection at top surface (forced+Free)
+                   %(outputs q"conv
+                   %Free Convection
+                   T_film = (T_amb + T(1,i))/2;
+                   beta = 1/T_film;
+                   Ral = abs((9.8*beta*(T(1,i) - T_amb)*L_gr^3)/(alpha_air*v));
+                   Nu_free = 0.52*Ral^(1/5);
+                  
+                   %Forced Convection
+                   Re = wind_speed*L_re/(v);
+                   A = 0.037*Re^(4/5)-0.664*Re^(1/2);
+                   Nu_forced = (0.037*Re^(4/5)-A)*Pr^(1/3);
+                   %Calculate total Convection
+                   Nu_total = Nu_forced+ Nu_free;
+                   h_bar = Nu_total*k/L_re;
+                   q_conv = A_sidewalk*h_bar*(T(1,i)-T_amb);
+                  
+                   %Need a function to calculate radiation heat transfer outputs q"rad
+                   F_cloud = 1 + 0.024*N-0.0035*N^2+0.00028*N^3;
+                   e_s = 0.787+0.764*log(T_dew/273)*F_cloud;
+                   T_sky = e_s^(.25)*T_amb;
+                   q_rad = A_sidewalk*emissivity*(5.67*10^(-8))*(T(1,i)^4-T_sky^4);
+                   q_rad_sun = A_sidewalk*solar_abs*q_raq_flux;
+                   q_total = q_rad+q_conv+q_rad_sun;
+                  
+                   for u = 1:length(nodes) %Loop Through Nodes to find temps at t+1
+                       if nodes(u) == 1 %Dirt nodes
+                           T(u,i+1) = T(u,i)+Fo_dirt*(T(u+1,i)+T(u-1,i)-2*T(u,i));
+                       elseif nodes(u) == 2 %dirt concrete interface
+                            T(u,i+1) = Fo_Jconc*T(u-1,i)+Fo_Jdirt*T(u+1,i)+ (1-Fo_Jconc-Fo_Jdirt)*T(u,i);
+                       elseif nodes(u) == 3 %concrete nodes
+                            T(u,i+1) = T(u,i)+Fo_conc*(T(u+1,i)+T(u-1,i)-2*T(u,i));
+                       elseif nodes(u) == 4 %surface node
+                            T(u,i+1) = T(u,i)+2*Fo_conc*(T(u+1,i)-T(u,i))+2*Fo_conc*q_total*nodal_spacing/(k_conc);
+                       elseif nodes(u) == 5 %insulated node
+                            T(u,i+1) = T(u,i);%+2*Fo_dirt*(T(u-1,i)-T(u,i));
+                       end
+                   end
+                end
+                figure()
+                hold on
+                title('Temp at Varying Depths')
+                xlabel('Time (min)')
+                ylabel('Temp (c)')
+                plot(1:length(T(1,:)),T(1,:))
+                plot(1:length(T(25,:)),T(25,:))
+                plot(1:length(T(50,:)),T(50,:))
+                plot(1:length(T(75,:)),T(75,:))
+                plot(1:length(T(length(nodes),:)),T(length(nodes),:))
+                legend('Surface','.25m', '.5m', '.75m', '1.14m')
+                hold off
+                figure()
+                surf(T)
+                shading interp
+                title('Temp Vs Depth Vs Time')
+                xlabel ('time(min)')
+                ylabel('Depth (cm)')
+                zlabel('Temp (c)')
+                figure()
+                plot(time_hours,T(1,1:(length(time_hours))))
+                title('Concrete Surface Temp')
+                xlabel('Time (hours)')
+                ylabel('Sidewalk Temp (c)')
+                axis([0 48 -inf inf])
+
+            """            
         
         elif section == "6: Recommendations":
             st.subheader("6. Recommendations")
